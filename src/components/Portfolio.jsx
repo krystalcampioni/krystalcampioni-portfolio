249d8496327b8ea6be1941355404914406c52517
Krystal Campioni <krystal.designer@gmail.com>
2016-11-20 12:24:40 -0200

    setup portfolio projects via jason request

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import imgPath from  '../images/krystal.png'
 import styles from '../styles/main.scss';
+import axios from 'axios';
 
 var Portfolio = React.createClass({
+  getInitialState: function() {
+    return {
+      projects: []
+    }
+  },
+
+  componentDidMount: function() {
+   var self = this;
+   this.serverRequest =
+     axios
+       .get("./data.json")
+       .then(function(result) {
+         console.log(result.data.projects);
+         self.setState({
+           jobs: result.data.projects
+         });
+       })
+       .catch(function (error) {
+         console.log(error);
+       })
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
   render: function(){
     return(
     <div className={styles.main__internal}>
