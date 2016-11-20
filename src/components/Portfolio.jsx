09880af18d0367e254d77c86a7ee573aa0b5a3f8
Krystal Campioni <krystal.designer@gmail.com>
2016-11-20 13:03:37 -0200

    implement portfolio projects filter

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from '../styles/main.scss';
 import axios from 'axios';
+import FlipMove from 'react-flip-move';
+import _ from 'lodash';
 
 var Portfolio = React.createClass({
   getInitialState: function() {
     return {
-      projects: []
+      projects: [],
+      allProjects: []
     }
   },
 
@@ -17,7 +20,8 @@ var Portfolio = React.createClass({
        .then(function(result) {
          console.log(result.data.projects);
          self.setState({
-           jobs: result.data.projects
+           projects: result.data.projects,
+           allProjects: result.data.projects
          });
        })
        .catch(function (error) {
@@ -29,10 +33,35 @@ var Portfolio = React.createClass({
     this.serverRequest.abort();
   },
 
+  filterBy: function(){
+    this.setState({
+      projects: _.filter(this.state.projects, ['category', 'Frontend'])
+    });
+  },
+
+  showAll: function(){
+    this.setState({
+      projects: this.state.allProjects
+    })
+  },
+
   render: function(){
+    var projectCards = this.state.projects.map(function(result, index) {
+      return (
+        <div key={result.link}>
+          <h1>{result.name}</h1>
+        </div>
+      );
+    });
+
     return(
     <div className={styles.main__internal}>
       <h2>Portfolio</h2>
+        <button onClick={this.showAll}> All </button>
+        <button onClick={this.filterBy}>Frontend</button>
+        <FlipMove easing="cubic-bezier(0, 0.7, 0.8, 0.1)">
+          {projectCards}
+         </FlipMove>
     </div>
     )
   }
