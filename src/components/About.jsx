5cee0aee35ddd5936effa5be7f1269591b4daf36
Krystal Campioni <krystal.designer@gmail.com>
2016-11-22 10:17:29 -0200

    place an astronaut helmet on me 👽

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from '../styles/main.scss';
-import ChatBubble from './ChatBubble'
-import Rocket from './Rocket'
+import imgPath from  '../images/krystal.png'
 
 var About = React.createClass({
   render: function(){
     return(
       <div className={styles.main__internal}>
         <h2>About</h2>
-      </div>
+          <div className={styles.about__bio}>
+            <img src={imgPath} />
+            <p className={styles.about__text}>Hello there! I’m Krystal.<br/>
+            I’ve been working with Front-end Development and Design for 8 years now. During this time, I’ve had the opportunity to work on the design and development of projects for clients in Brazil, Canada and Europe</p>
+          </div>
+    </div>
     )
   }
 });
