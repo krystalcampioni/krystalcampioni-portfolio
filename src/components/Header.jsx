be76e4ee35b32b98a93436fe6f9898a278faa1c8
Krystal Campioni <krystal.designer@gmail.com>
2016-11-24 20:06:06 -0200

    hide dektop menu on mobile and vice versa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import imgPath from  '../images/logo.svg'
 import styles from '../styles/main.scss';
-import { Link, IndexLink } from 'react-router'
-
-const ACTIVE = { borderBottom: '8px solid #FE0170' }
+import { Link, IndexLink } from 'react-router';
+import Nav from './Nav';
 
 var Header = React.createClass({
   render: function(){
@@ -18,24 +17,12 @@ var Header = React.createClass({
               <span></span>
               <label htmlFor="change-hamburguer"></label>
             </a>
-            <div className="menu">
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-              <a href="#">Item</a>
-              <p><a href="#">Item</a></p>
+            <div className="menuMobile">
+              <Nav />
             </div>
           </div>
-          <Link to="/"><img src={imgPath} className={styles.header__logo} /></Link>
-            <nav className={styles.menu}>
-                <IndexLink activeStyle={ACTIVE} className={styles.menu__item} to="/">Home</IndexLink>
-                <Link  activeStyle={ACTIVE} className={styles.menu__item} to="/portfolio">Portfolio</Link>
-                <Link  activeStyle={ACTIVE} className={styles.menu__item} to="/about">About Me</Link>
-                <a className={styles.menu__item} href="https://br.linkedin.com/in/krystalcampioni/en" target="_blank">Resume</a>
-                <a className={styles.menu__item} href="https://medium.com/@krystalcampioni" target="_blank">Blog</a>
-                <Link  activeStyle={ACTIVE} className={styles.menu__item} to="/contact">Contact</Link>
-            </nav>
+          <IndexLink to="/"><img src={imgPath} className={styles.header__logo} /></IndexLink>
+          <nav className={styles.menuDesktop}><Nav /></nav>
         </div>
       </header>
       <div className={styles.rocket__wrapper}>
