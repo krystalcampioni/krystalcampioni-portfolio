6847e9ef9bc2932cbec8a53f9f38b6b357fafd8d
Krystal Campioni <krystal.designer@gmail.com>
2016-11-29 21:06:16 -0200

    imported linkedin resume as json

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,8 +6,7 @@ import _ from 'lodash';
 var Portfolio = React.createClass({
   getInitialState: function() {
     return {
-      projects: [],
-      allProjects: []
+      resume: []
     }
   },
 
@@ -17,10 +16,8 @@ var Portfolio = React.createClass({
      axios
        .get("./resume.json")
        .then(function(result) {
-         console.log(result)
          self.setState({
-           projects: result.data.projects,
-           allProjects: result.data.projects
+           resume: result.data
          });
        })
        .catch(function (error) {
@@ -29,11 +26,45 @@ var Portfolio = React.createClass({
   },
 
   render: function(){
-    return(
-    <div className={styles.main__internal}>
-      <h2 className={styles.about__title}>Resume</h2>
-    </div>
-    )
+    if(this.state.resume.length === 0){
+      return <h1>"loading"</h1>
+    }
+    else {
+      console.log(this.state.resume)
+
+      let certifications = this.state.resume.certifications.values.map(function(certification) {
+        return (
+        <div key={certification.id} className={styles.resume__certification}>{certification.name}</div>
+        );
+      });
+
+      let positions = this.state.resume.positions.values.map(function(position, index) {
+        return (
+        <div key={index} className={styles.resume__certification}>
+          <h4>{position.company.name}</h4>
+          <p className={styles.resume__text}>{position.summary}</p>
+        </div>
+        );
+      });
+
+      return(
+      <div className={styles.main__internal}>
+        <h2 className={styles.about__title}>Resume</h2>
+        <div className={styles.about__bio}>
+          <img src={this.state.resume.pictureUrl} />
+          <h1 className={styles.resume__title}>{this.state.resume.formattedName}</h1>
+          <h3>{this.state.resume.headline}</h3>
+          <div className={styles.resume__contact}>{this.state.resume.emailAddress}</div>
+          <div className={styles.resume__contact}>Sao Paulo, Brazil</div>
+        </div>
+        <div className={styles.about__col}>
+          {positions}
+          <hr />
+          {certifications}
+        </div>
+      </div>
+      )
+    }
   }
 });
 
